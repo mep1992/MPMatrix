a9631ca47b27846cc81c37f7b0bb6e6f145ba292
Michael Parker <me.parker1992@gmail.com>
2019-12-15 15:53:35 +0100

    Fix type in test

--- test/MPMatrixSpec.hs
+++ test/MPMatrixSpec.hs
@@ -5,13 +5,12 @@ import MPMatrix
 
 main :: IO ()
 main = hspec $ do
-  describe "scalarMult" $ do
+  describe "Matrix Functions" $ do
     it "returns the scalar multiple of a given Matrix" $
       scalarMult 2 (Matrix [Row [1,1], Row [1,1]]) `shouldBe` (Matrix [Row [2,2], Row [2,2]])
 
     it "returns the addition of two Matricies" $
       mAdd (Matrix [Row [1,1], Row [1,1]]) (Matrix [Row [1,1], Row [1,1]]) `shouldBe` (Matrix [Row [2,2], Row [2,2]])
-
 
     it "returns the subtraction of two Matricies" $
       mSub (Matrix [Row [1,1], Row [1,1]]) (Matrix [Row [1,1], Row [1,1]]) `shouldBe` (Matrix [Row [0,0], Row [0,0]])
