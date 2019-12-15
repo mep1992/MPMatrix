dc20209a1d1d7ffb381ac1ba97ece2b9931a6e57
Michael Parker <me.parker1992@gmail.com>
2019-12-15 11:36:39 +0100

    Add custom show to print data more nicely

--- MPMatrix.hs
+++ MPMatrix.hs
@@ -4,8 +4,23 @@ module MPMatrix
 , scalarMult
 ) where
 
-data Row a = Row [a] deriving (Show)
-data Matrix a = Matrix [Row a] deriving (Show)
+import Data.Text (strip, pack, unpack)
+
+data Row a = Row [a]
+data Matrix a = Matrix [Row a]
+
+showRow :: (Num a, Show a) => Row a -> String
+showRow (Row x) = "( " ++ (init $ tail $ show x) ++ " )"
+
+//TODO figure out why this function has a \n at the start to avoid having the Text package
+showMatrix :: (Num a, Show a) => Matrix a -> String
+showMatrix (Matrix rows) = unpack $ strip $ pack $ foldl (\x y -> x ++ "\n" ++ (show y)) "" rows
+
+instance (Num a, Show a) => Show (Row a) where
+    show = showRow
+
+instance (Num a, Show a) => Show (Matrix a) where
+    show = showMatrix
 
 mAdd :: (Num a) => Matrix a -> Matrix a -> Matrix a
 mAdd x y = combineMatrix (+) x y
@@ -16,7 +31,7 @@ mSub x y = combineMatrix (-) x y
 scalarMult :: (Num a) => a -> Matrix a -> Matrix a
 scalarMult x (Matrix rows) = Matrix (map (scalarRowMult x) rows)
 
-scalarRowMult ::(Num a) => a -> Row a -> Row a
+scalarRowMult :: (Num a) => a -> Row a -> Row a
 scalarRowMult x (Row xs) = Row (map (*x) xs)
 
 prependRow :: (Num a) => Row a -> Matrix a -> Matrix a
